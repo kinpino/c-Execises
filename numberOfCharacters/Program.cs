219cbf81d05e5a625295388e3142f1e956e762e5
kocha k.c <kings.okocha@gmail.com>
2018-04-15 19:32:22 +0100

    Add project files.

---
+++ numberOfCharacters/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberOfCharacters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Character length count
+            String userInputString;
+            Console.WriteLine("Please Enter your input String");
+            userInputString = Console.ReadLine();
+      
+            //Incase Empty input is entered.
+            if (userInputString.Length == 0 || userInputString == " ")
+            {
+                Console.WriteLine("Please enter a character");
+                
+            }
+            else
+            {
+                Console.WriteLine(userInputString + " has " + userInputString.Length + " characters.");
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ printingQuotes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace printingQuotes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print Quotes
+            //Creating variables
+            String userQuote;
+            String author;
+            String output;
+            //User Instructions and getting user input
+            Console.WriteLine("Please, what is the quote?");
+            userQuote = Console.ReadLine();
+            Console.WriteLine("Please, who is the author?");
+            author = Console.ReadLine();
+            // Console.WriteLine(author + says, " + "\" + userQuote + "\"); 
+            Console.WriteLine(author + " says, \""+ userQuote +"\"");
+            Console.WriteLine($"{author} says, \"{userQuote}\"");
+        }
+    }
+}
